c3ec1f5c4fdb3a67bcf3aaf62e233ff0c45c4f22
Viner108 <andrukovichl@mail.ru>
2024-02-15 12:36:00 +0300

    Initial commit

---
+++ src/test/java/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+    @Test
+    void  test(){
+        assertTrue(false);
+    }
+}
